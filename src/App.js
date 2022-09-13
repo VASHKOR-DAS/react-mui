0c1516c50c3ac1a3a813effad348a0bcf975cd49
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-13 14:25:06 +0600

    show card in dynamically data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Appbar from "./components/Appbar/Appbar";
+import News from "./components/News/News";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=35acf0f6d5d24ea5890c99906a98ec6f'
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
+  }, [])
   return (
     <div>
-     
+      <Appbar></Appbar>
+
+      <h2>{articles.length}</h2>
+      <br /><br />
+
+      {
+        articles.map(article =>
+          <News
+            article = {article}
+          >
+          </News>)
+      }
+
     </div>
   );
 }
